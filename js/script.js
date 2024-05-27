4151fd47eac231541538484c2141467f7554f3c6
Emma-Ssali <ssaliemmanuel2000@gmail.com>
2024-05-27 20:58:21 +0300

    portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* === typing animation === */
+var typed = new Typed(".typing",{
+    strings:["Python Programmer", "Website Designer", "Computer Technician"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true,
+})
